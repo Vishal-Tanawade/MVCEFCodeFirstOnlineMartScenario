e5e0b55cb0d3028e37bcdaa6d403df2ffc9bcdb7
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-28 15:07:40 +0530

    EF Installed -DbContext Class Created -Connection String Added

---
+++ MVCEFCodeFirstOnlineMartScenario/AppDBContext.cs
@@ -0,0 +1,29 @@
+﻿using MVCEFCodeFirstOnlineMartScenario.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCEFCodeFirstOnlineMartScenario
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext() : base("name=OnlineMartConnnectionString")
+        {
+
+        }
+
+
+        public DbSet<UserType> UserTypes { get; set; }
+        public DbSet<SecurityQuestion> SecurityQuestions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+    }
+}
